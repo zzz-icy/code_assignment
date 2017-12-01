301d2a436bf1fd89eb144d2448486884a4526814
Han Zhang <han.zhang1129@gmail.com>
2017-11-30 19:41:50 -0600

    header style and basic function test

---
+++ js/script.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+
+    var fingers = $(".display-fingers");
+    var toes = $('.display-toes');
+
+    $("#restart").on('click', () => {
+
+        fingers.css("background-color", "yellow");
+
+    });
+
+
+});
