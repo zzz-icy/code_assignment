3e94fb10e108827ded1d27c1cb8d422089ef8589
Han Zhang <han.zhang1129@gmail.com>
2017-11-30 23:37:05 -0600

    layout finished

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ $(document).ready(function () {
 
     var fingers = $(".display-fingers");
     var toes = $('.display-toes');
+    
+    // get num of seconds
+    var seconds;
+
+    $(".time-confirm-button ").find("button").on('click', () => {
+        seconds = $('#seconds').val();
+    });
+
+    $(".time-reset-button").find("button").on('click', () => {
+        $('#seconds').val('');
+
+    });
+
+
 
     $("#restart").on('click', () => {
 
